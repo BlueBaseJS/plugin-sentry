f2305ff03814acb8dec3511e960217c1e43e9da5
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-01-01 21:13:56 +0500

    feat: Updated to latest Sentry API
    BREAKING CHANGE: Updated to latest Sentry SDK. Also, the plugin no longer inits Sentry. That can be done in bluebase.ts file directly.

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,58 @@
-import { BootOptions, createPlugin } from '@bluebase/core';
+import * as Sentry from '@sentry/react-native';
 
-import Raven from 'raven-js';
 import { VERSION } from './version';
+import { createPlugin } from '@bluebase/core';
 
 export default createPlugin({
 	key: 'plugin-senetry',
 	name: 'Sentry Plugin',
 	version: VERSION,
 
-	description: 'A BlueBase plugin boilerplate!',
-
-	defaultConfigs: {
-		'plugin.sentry.url': null,
-	},
+	description: 'Sends BlueBase Logger messages to Sentry',
 
 	filters: {
-		'bluebase.boot.end': (bootOptions: BootOptions, _ctx: any, BB) => {
-			const url = BB.Configs.getValue('plugin.sentry.url');
-			if (!url) {
-				Raven.config(url).install();
-			}
-			return bootOptions;
-		},
 		'bluebase.logger.debug': (message: string, data: object) => {
-			Raven.captureMessage(message, { logger: 'info', ...data });
+			Sentry.withScope(scope => {
+				scope.setLevel(Sentry.Severity.Debug);
+				scope.setExtras(data);
+				Sentry.captureException(message);
+			});
 			return message;
 		},
+
 		'bluebase.logger.error': (message: string, data: object) => {
-			Raven.captureMessage(message, { logger: 'error', ...data });
+			Sentry.withScope(scope => {
+				scope.setLevel(Sentry.Severity.Error);
+				scope.setExtras(data);
+				Sentry.captureException(message);
+			});
 			return message;
 		},
+
 		'bluebase.logger.info': (message: string, data: object) => {
-			Raven.captureMessage(message, { logger: 'info', ...data });
+			Sentry.withScope(scope => {
+				scope.setLevel(Sentry.Severity.Info);
+				scope.setExtras(data);
+				Sentry.captureException(message);
+			});
 			return message;
 		},
+
 		'bluebase.logger.log': (message: string, data: object) => {
-			Raven.captureMessage(message, { logger: 'log', ...data });
+			Sentry.withScope(scope => {
+				scope.setLevel(Sentry.Severity.Log);
+				scope.setExtras(data);
+				Sentry.captureMessage(message);
+			});
 			return message;
 		},
+
 		'bluebase.logger.warn': (message: string, data: object) => {
-			Raven.captureMessage(message, { logger: 'warn', ...data });
+			Sentry.withScope(scope => {
+				scope.setLevel(Sentry.Severity.Warning);
+				scope.setExtras(data);
+				Sentry.captureException(message);
+			});
 			return message;
 		},
 	},
